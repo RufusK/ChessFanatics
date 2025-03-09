e2783c3efc565d55b5429ea58c3f42b350857e85
RufusK <jimzitrone@gmail.com>
2025-03-09 21:35:20 +0100

    rounds done

--- Core/Models/Game.cs
+++ Core/Models/Game.cs
@@ -2,11 +2,11 @@ namespace Core.Models;
 
 public class Game: BaseEntity
 {
-    public int PlayerWhiteId { get; set; }
-    public Player PlayerWhite { get; set; }
+    public int? PlayerWhiteId { get; set; }
+    public Player? PlayerWhite { get; set; }
     public int? PlayerBlackId { get; set; }
     public Player? PlayerBlack { get; set; }
-    public GameResult? Result { get; set; } = GameResult.None;
+    public GameResult Result { get; set; } = GameResult.None;
     
     public int TournamentRoundId { get; set; }
     public TournamentRound TournamentRound { get; set; }
